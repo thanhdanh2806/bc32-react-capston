e35db4b8e724855c6f09d84796237712fcf866c0
thanhdanh2806 <oldwolf2k@gmail.com>
2022-11-13 17:33:41 +0700

    bc32-react-capston

---
+++ src/modules/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Banner from "./Banner";
+import Showing from "./Showing";
+
+const Home = () => {
+  return (
+    <>
+      H
+      <Banner />
+      <Showing />
+    </>
+  );
+};
+
+export default Home;
---
+++ src/modules/Home/Showing/Showing.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Showing = () => {
+  return <div>Showing</div>;
+};
+
+export default Showing;
